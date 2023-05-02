e024545db944e5e8e648c43f13a6b317d13612c2
Dedo_Finger2 <antonioimportant@gmail.com>
2023-05-02 18:05:42 -0300

    Controllers abstraction ready

--- app/Controller/AccountController.class.php
+++ app/Controller/AccountController.class.php
@@ -1,5 +1,63 @@
 <?php
 
-namespace App\Controller {
-    
+namespace App\Controller;
+
+use Exception;
+
+class AccountController
+{
+    /**
+     * Cria um formulário para criação de uma nova conta
+     */
+    public function createForm()
+    {
+
+    }
+
+    /**
+     * Processa os dados na adição de um nova conta
+     * @param array $data - Dados para criação de um nova conta
+     * @return  int - ID da nova conta criado 
+     */
+    public function storeData($data)
+    {
+
+    }
+
+    /**
+     * Cria um formulário para edição de uma conta
+     * @param int $account_id - ID da conta que vai ser editado
+     */
+    public function editForm($account_id)
+    {
+
+    }
+
+    /**
+     * Processa os dados na edição de uma conta
+     * @param array $dados - Novos dados que vão substituir os antigos dados da conta
+     * @param int $account_id - ID da conta que vai ser editado
+     */
+    public function updateData($account_id, $dados)
+    {
+
+    }
+
+    /**
+     * Abre um modal para confirmar deleção da conta
+     * @param int $account_id - ID da conta que vai ser deletado
+     */
+    public function deleteForm()
+    {
+
+    }
+
+    /**
+     * Remove uma conta do banco de dados
+     * @param int $account_id
+     */
+    public function deleteData($account_id)
+    {
+
+    }
 }
